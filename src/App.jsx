4f4c30c2db418a630c6de585997d8f3e1e1aa0d0
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-05-13 11:00:40 +0200

    sass setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "./scss/style.scss";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <p>Johnny</p>
       
     </div>
   );
