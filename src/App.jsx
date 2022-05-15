2a7cc5ff268a86a43eb35dfa0fe8de4e02f729a5
Magnus <magn902m@stud.kea.dk>
2022-05-15 22:57:06 +0200

    made a flip ticket

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import "./scss/style.scss";
 import Header from "./components/Header";
+import Ticket from "./components/Ticket";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <p>Johnny</p>
-      
+      <section id="ticket_container">
+        <Ticket />
+        <Ticket />
+      </section>
     </div>
   );
 }
