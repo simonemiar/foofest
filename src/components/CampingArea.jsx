17cd7de94837d3053595964056cdb0cdfc7d3db0
anders iversen <ande397c@stud.kea.dk>
2022-05-15 19:13:32 +0200

    Added function, get value from radio buttons

--- src/components/CampingArea.jsx
+++ src/components/CampingArea.jsx
@@ -1,43 +1,57 @@
 export default function CampingArea() {
+  function handleOnChange(e) {
+    const selectedOption = e.target.value;
+    console.log("selected camping option is:", selectedOption);
+  }
+
   return (
     <section id="camping-area_section">
       <div id="top-bar">
         <p>Camping area</p>
       </div>
+
       <div className="info">
         <p>Pick your camping area</p>
       </div>
+
       <section id="camping-area_main-content">
         <article className="area_options">
           {/* Svartheim */}
           <div className="option">
-            <input type="radio" id="svartheim" name="area" value="svartheim"></input>
+            <input type="radio" id="svartheim" name="area" value="svartheim" onChange={handleOnChange}></input>
             <label htmlFor="svartheim">Svartheim</label>
             <p className="spots_left">Spots left: xxx</p>
           </div>
 
           {/* Nilfheim */}
-          <div className="option">
-            <input type="radio" id="nilfheim" name="area" value="nilfheim"></input>
+          <article className="option">
+            <input type="radio" id="nilfheim" name="area" value="nilfheim" onChange={handleOnChange}></input>
             <label htmlFor="nilfheim">Nilfheim</label>
             <p className="spots_left">Spots left: xxx</p>
-          </div>
-          <div className="option">
-            <input type="radio" id="helheim" name="area" value="helheim"></input>
+          </article>
+
+          {/* Helheim */}
+          <article className="option">
+            <input type="radio" id="helheim" name="area" value="helheim" onChange={handleOnChange}></input>
             <label htmlFor="helheim">Helheim</label>
             <p className="spots_left">Spots left: xxx</p>
-          </div>
-          <div className="option">
-            <input type="radio" id="muspelheim" name="area" value="muspelheim"></input>
+          </article>
+
+          {/* Muspelheim */}
+          <article className="option">
+            <input type="radio" id="muspelheim" name="area" value="muspelheim" onChange={handleOnChange}></input>
             <label htmlFor="muspelheim">Muspelheim</label>
             <p className="spots_left">Spots left: xxx</p>
-          </div>
-          <div className="option">
-            <input type="radio" id="alfheim" name="area" value="alfheim"></input>
+          </article>
+
+          {/* Alfheim */}
+          <article className="option">
+            <input type="radio" id="alfheim" name="area" value="alfheim" onChange={handleOnChange}></input>
             <label htmlFor="alfheim">Alfheim</label>
             <p className="spots_left">Spots left: xxx</p>
-          </div>
+          </article>
         </article>
+
         <article className="illustration">
           <div className="svg_img">I'm a SVG img!!</div>
         </article>
