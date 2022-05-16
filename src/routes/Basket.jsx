bf66fd36fecdb9b60fca214d75325862ce344f0a
Magnus <magn902m@stud.kea.dk>
2022-05-16 13:22:38 +0200

    made new/edit components to booking flow

--- src/routes/Basket.jsx
+++ src/routes/Basket.jsx
@@ -1,3 +1,25 @@
-export default function Basket(){
-    return <h1>Basket</h1>
+// import Header from "./components/Header";
+
+import { useState } from "react";
+
+import TicketDetails from "../components/TicketDetails";
+import PersonInfo from "../components/PersonInfo";
+// import Header from "../components/Header";
+
+export default function Basket() {
+  const [showPersonInfo, setShowPersonInfo] = useState(false);
+
+  return (
+    <main>
+      {/* <button onClick={() => showPersonInfo(true)}>Person info</button>
+
+      <button onClick={() => setShowPersonInfo(false)}>Details</button> */}
+
+      {showPersonInfo ? (
+        <PersonInfo showPersonInfo={showPersonInfo} setShowPersonInfo={setShowPersonInfo} />
+      ) : (
+        <TicketDetails showPersonInfo={showPersonInfo} setShowPersonInfo={setShowPersonInfo} />
+      )}
+    </main>
+  );
 }
