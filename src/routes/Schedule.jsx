c73f77590cb7c57156126540de97201bd9463713
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-05-16 20:30:30 +0200

    schedule dropdown works

--- src/routes/Schedule.jsx
+++ src/routes/Schedule.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 export default function Schedule(){
     return (
     <div>
@@ -14,13 +16,17 @@ export default function Schedule(){
 }
 
 function Details(){
+    const [showSchedule, setScheduleMore] = useState(false);
     
     return (
         <div>
-        <details id="details_section">
-        <summary><h3>MONDAY</h3></summary>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis non rerum ipsam porro sed a tempore optio, architecto voluptatibus voluptate deserunt mollitia quas numquam dolor vero aliquid nam, magnam tenetur. Ipsum officia eum explicabo iusto ea porro ab quo voluptatibus dolor magnam, nulla corporis maiores similique enim? Illum, reiciendis aperiam?</p>
-        </details>
+        
+        <button id="details_button" onClick={()=> setScheduleMore((old) => !old)}><summary><h3>MONDAY</h3></summary></button>
+        <section id="details_section" style={{display: showSchedule ? "block" : "none"}}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis non rerum ipsam porro sed a tempore optio, architecto voluptatibus voluptate deserunt mollitia quas numquam dolor vero aliquid nam, magnam tenetur. Ipsum officia eum explicabo iusto ea porro ab quo voluptatibus dolor magnam, nulla corporis maiores similique enim? Illum, reiciendis aperiam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis non rerum ipsam porro sed a tempore optio, architecto voluptatibus voluptate deserunt mollitia quas numquam dolor vero aliquid nam, magnam tenetur. Ipsum officia eum explicabo iusto ea porro ab quo voluptatibus dolor magnam, nulla corporis maiores similique enim? Illum, reiciendis aperiam?</p>
+
+        </section>
+        
 
         </div>
       );
