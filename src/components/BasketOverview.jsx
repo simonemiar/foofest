231820a85485159664621c6a37bb84c7cef72cd7
Magnus <magn902m@stud.kea.dk>
2022-05-17 13:15:31 +0200

    fix no btn on BasketOverview

--- src/components/BasketOverview.jsx
+++ src/components/BasketOverview.jsx
@@ -33,59 +33,61 @@ export default function BasketOverview(props) {
     }
   }
   return (
-    <section className="basket_overview">
-      <h2>Basket overview</h2>
-      <div className="top-bar">
-        <p>Ticket</p>
-        <p>Quanity</p>
-        <p>Price</p>
-      </div>
+    <>
+      <section className="basket_overview">
+        <h2>Basket overview</h2>
+        <div className="top-bar">
+          <p>Ticket</p>
+          <p>Quanity</p>
+          <p>Price</p>
+        </div>
 
-      <section className="overview_content">
-        <article className="ticket_row">
-          <div className="ticket_name">
-            <p id="ticket_type">Regular Ticket</p>
-          </div>
+        <section className="overview_content">
+          <article className="ticket_row">
+            <div className="ticket_name">
+              <p id="ticket_type">Regular Ticket</p>
+            </div>
 
-          <div className="ticket_amount">
-            <button onClick={removeTicket}>-</button>
-            <span id="amount_ticket">{ticketCount}</span>
-            <button onClick={addTicket}>+</button>
-          </div>
-          <div className="ticket_price">
-            <p className="total_ticket_price">{totalTicketPrize} kr.</p>
-          </div>
-        </article>
+            <div className="ticket_amount">
+              <button onClick={removeTicket}>-</button>
+              <span id="amount_ticket">{ticketCount}</span>
+              <button onClick={addTicket}>+</button>
+            </div>
+            <div className="ticket_price">
+              <p className="total_ticket_price">{totalTicketPrize} kr.</p>
+            </div>
+          </article>
 
-        <article className="tent_row">
-          <div className="tent_name">
-            <p>3 person tent</p>
-            <p>Remove</p>
-          </div>
-          <div className="tent_ui">
-            <button onClick={removeThreeTent}>-</button>
-            <span id="amount_tent">{threePersonTent}</span>
-            <button onClick={addThreeTent}>+</button>
-          </div>
+          <article className="tent_row">
+            <div className="tent_name">
+              <p>3 person tent</p>
+              <p>Remove</p>
+            </div>
+            <div className="tent_ui">
+              <button onClick={removeThreeTent}>-</button>
+              <span id="amount_tent">{threePersonTent}</span>
+              <button onClick={addThreeTent}>+</button>
+            </div>
 
-          <div className="tent_total">
-            <p>{totalThreeTent} kr.</p>
-          </div>
-        </article>
+            <div className="tent_total">
+              <p>{totalThreeTent} kr.</p>
+            </div>
+          </article>
 
-        <article className="fee_row">
-          <div className="fee_name">
-            <p>Booking fee</p>
-          </div>
+          <article className="fee_row">
+            <div className="fee_name">
+              <p>Booking fee</p>
+            </div>
 
-          <div className="fee_total">
-            <p>25 kr.</p>
-          </div>
-        </article>
-        <article className="total_bar">
-          <p>Total ({totalItems} items)</p>
-          <p className="total_price">{totalPrice} kr.</p>
-        </article>
+            <div className="fee_total">
+              <p>25 kr.</p>
+            </div>
+          </article>
+          <article className="total_bar">
+            <p>Total ({totalItems} items)</p>
+            <p className="total_price">{totalPrice} kr.</p>
+          </article>
+        </section>
       </section>
       <div className="booking_flow_nav">
         <button
@@ -107,6 +109,6 @@ export default function BasketOverview(props) {
           Continue
         </button>
       </div>
-    </section>
+    </>
   );
 }
