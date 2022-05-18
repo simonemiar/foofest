dafb35e5283b7146ac36d1607897051e1dc69306
anders iversen <ande397c@stud.kea.dk>
2022-05-18 09:23:42 +0200

    Code cleanup

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -5,16 +5,16 @@ export default function ProgressBar() {
   return (
     <section class="progressbar_container">
       <ul class="steps_container">
-        <li class="step-item  current_item">
+        <li class="step-item  ">
           <span class="progress-count">1</span>
         </li>
-        <li class="step-item ">
+        <li class="step-item  ">
           <span class="progress-count">2</span>
         </li>
         <li class="step-item   ">
           <span class="progress-count">3</span>
         </li>
-        <li class="step-item  ">
+        <li class="step-item  current_item">
           <span class="progress-count">4</span>
         </li>
       </ul>
