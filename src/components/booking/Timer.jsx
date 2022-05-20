ab0d75277f35ad2889696911bf79a227f4b7c720
anders iversen <ande397c@stud.kea.dk>
2022-05-20 09:41:29 +0200

    Timer redirects user back to tickets page

--- src/components/booking/Timer.jsx
+++ src/components/booking/Timer.jsx
@@ -2,11 +2,16 @@
 // https://www.codegrepper.com/code-examples/javascript/react+countdown+timer+minutes+seconds
 import React from "react";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Timer = (props) => {
-  const { initialMinute = 2, initialSeconds = 10 } = props;
+  const { initialMinute = 0, initialSeconds = 10 } = props;
   const [minutes, setMinutes] = useState(initialMinute);
   const [seconds, setSeconds] = useState(initialSeconds);
+
+  let navigate = useNavigate();
+  //https://www.youtube.com/watch?v=tiAlSpyWIDs&ab_channel=PedroTech
+
   useEffect(() => {
     let myInterval = setInterval(() => {
       if (seconds > 0) {
@@ -26,16 +31,15 @@ const Timer = (props) => {
     };
   });
 
-  function ended() {
-    console.log("time is up!");
-
-    return <h3>TIME LEFT: 0:00</h3>;
+  function timeExpired() {
+    console.log("Your session has expired!");
+    // navigate("/tickets");
   }
 
   return (
     <div className="timer_container">
       <h2>BASKET</h2>
-      {minutes === 0 && seconds === 0 ? ended() : <h3>TIME LEFT: {`${minutes}:${seconds < 10 ? `0${seconds}` : seconds} `}</h3>}
+      {minutes === 0 && seconds === 0 ? timeExpired() : <h3>TIME LEFT: {`${minutes}:${seconds < 10 ? `0${seconds}` : seconds} `}</h3>}
     </div>
   );
 };
