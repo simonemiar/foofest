3aacb52636f7c25141d4d826a78ba528cc7726e2
anders iversen <ande397c@stud.kea.dk>
2022-05-30 21:22:51 +0200

    Implementet new ticket and map svg

--- src/components/booking/CampingArea.jsx
+++ src/components/booking/CampingArea.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 
+import Map from "../../assets/svg/map.svg";
+
 import AreaOption from "./AreaOption";
 
 export default function CampingArea() {
@@ -41,17 +43,7 @@ export default function CampingArea() {
         {/* Link to dk map svg: https://www.amcharts.com/svg-maps/?map=denmark */}
         <article className="illustration">
           <div className="svg_img">
-            <svg
-              className="svg_img"
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              // className="bi bi-geo-alt-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
-            </svg>
+            <img className="area_map" src={Map} alt={Map} />
           </div>
         </article>
       </section>
