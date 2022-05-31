e883b301398786e1c70390c889df99133c11a3bd
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-05-31 10:42:09 +0200

    implement favicon

--- src/components/schedule/Act.jsx
+++ src/components/schedule/Act.jsx
@@ -6,51 +6,57 @@ export default function Act(props) {
           {props.act.start}-{props.act.end}
         </th>
         <td className="artist">
-          {props.act.start}-{props.act.end}
+        <span>{props.act.act}</span>
+        <span>{props.act.start}-{props.act.end}</span>
           {props.act.act !== "break" ? (
             <button
               onClick={() => {
                 props.findBandDetails(props.act);
                 props.setStage("Midgard");
               }}
-            >
-              {props.act.act}
+            > 
+              read more
             </button>
           ) : (
-            "Break"
+            ""
           )}
+          
         </td>
 
         <td className="artist">
-          {props.jAct.start}-{props.jAct.end}
-          {props.jAct.act !== "break" ? (
+        <span>{props.jAct.act}</span>
+        <span>{props.jAct.start}-{props.jAct.end}</span>
+        {props.jAct.act !== "break" ? (
             <button
               onClick={() => {
                 props.findBandDetails(props.jAct);
                 props.setStage("Jotunheim");
               }}
             >
-              {props.jAct.act}
+              read more
             </button>
           ) : (
-            "Break"
+            ""
           )}
+          
         </td>
 
         <td className="artist">
-          {props.vAct.start}-{props.vAct.end}
-          {props.vAct.act !== "break" ? (
+        <span>{props.vAct.act}</span>
+        <span>{props.vAct.start}-{props.vAct.end}</span>
+        {props.vAct.act !== "break" ? (
             <button
               onClick={() => {
                 props.findBandDetails(props.vAct);
                 props.setStage("Vanaheim");
               }}
             >
-              {props.vAct.act}
+              read more
             </button>
           ) : (
-            "Break"
+            ""
           )}
+          
         </td>
       </tr>
     </>
