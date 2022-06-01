eee8f288a1544b089741e6e1e6f80a8852769b0b
Magnus <magn902m@stud.kea.dk>
2022-06-01 23:38:53 +0200

    improved frontpage aad inset logo

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import logoOrange from "../../assets/svg/logo_orange.svg";
 
 function NavBar() {
   const [click, setClick] = useState(false);
@@ -8,34 +9,35 @@ function NavBar() {
   return (
     <>
       <nav className="navbar">
-        <div className="nav-container">
-          <NavLink to="/" className="nav-logo">
-            FooFest
-            <i className="fas fa-code"></i>
-          </NavLink>
+        {/* <div className="nav-container"> */}
+        <NavLink to="/" className="nav-logo">
+          <img src={logoOrange} alt="FooFest logo" />
+          {/* FooFest
+            <i className="fas fa-code"></i> */}
+        </NavLink>
 
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <NavLink to="/" className="nav-links active" onClick={handleClick}>
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/schedule" className="nav-links active" onClick={handleClick}>
-                Schedule
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/tickets" className="nav-links active" onClick={handleClick}>
-                Tickets
-              </NavLink>
-            </li>
-          </ul>
-          {/* BURGER MENU ICON  */}
-          <div className="nav-icon" onClick={handleClick}>
-            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
-          </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <NavLink to="/" className="nav-links active" onClick={handleClick}>
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/schedule" className="nav-links active" onClick={handleClick}>
+              Schedule
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to="/tickets" className="nav-links active" onClick={handleClick}>
+              Tickets
+            </NavLink>
+          </li>
+        </ul>
+        {/* BURGER MENU ICON  */}
+        <div className="nav-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
+        {/* </div> */}
       </nav>
     </>
   );
