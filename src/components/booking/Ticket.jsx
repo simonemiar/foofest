381953727fe36ece878373aace71ba0650e0b51d
Magnus <magn902m@stud.kea.dk>
2022-06-03 04:44:19 +0200

    remove card flip and add party.js to tickets

--- src/components/booking/Ticket.jsx
+++ src/components/booking/Ticket.jsx
@@ -1,16 +1,17 @@
-import { useState, useContext } from "react";
-import ReactCardFlip from "react-card-flip";
+import { useContext } from "react";
+// import ReactCardFlip from "react-card-flip";
 import { Link } from "react-router-dom";
+import party from "party-js";
 
 import { TicketBasketContext } from "../../contexts/TicketBasketContext";
 
 export default function Ticket(props) {
-  const [isFlipped, setIsFlipped] = useState(false);
+  // const [isFlipped, setIsFlipped] = useState(false);
   const { setTicketBasket } = useContext(TicketBasketContext);
 
-  function handleClick(e) {
-    setIsFlipped((prevState) => !prevState);
-  }
+  // function handleClick(e) {
+  //   setIsFlipped((prevState) => !prevState);
+  // }
 
   // Here are we updating the ticketBasket state in the context
   // With the ticketType, ticketPrice and reseting the tent amount
@@ -31,44 +32,56 @@ export default function Ticket(props) {
   return (
     <article className="ticket">
       {/* We are using React-card-flip and it only works if there is two boxing inside it. */}
-      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-        <div className="ticket_svg">
-          <div className="ticket_content">
-            <h2>{props.ticketType}</h2>
-            <h3>FooFest 2022</h3>
-            <h4>{props.ticketPrice} DKK</h4>
-            <div className="ticket_btn">
-              {/* If the btn is clicked, the card will turn to the other side */}
-              <button type="primary" onClick={handleClick}>
-                Read more
-              </button>
-              OR
-              {/* Here are we adding the selected ticket to the ticketBasket, and going the the basket page. */}
-              <Link onClick={addTicketTypeToBasket} to="/basket">
-                Buy
-              </Link>
-            </div>
+      {/* <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical"> */}
+      <div className="ticket_svg">
+        <div className="ticket_content">
+          <h2>{props.ticketType}</h2>
+          <h3>FooFest 2022</h3>
+          <h4>{props.ticketPrice} DKK</h4>
+          <div className="ticket_btn">
+            {/* If the btn is clicked, the card will turn to the other side */}
+            {/* <button type="primary" onClick={handleClick}>
+              Read more
+            </button>
+            OR */}
+            {/* Here are we adding the selected ticket to the ticketBasket, and going the the basket page. */}
+            <Link
+              onClick={(e) => {
+                party.confetti(e.target, { count: party.variation.range(100, 200) });
+                addTicketTypeToBasket();
+              }}
+              to="/basket"
+            >
+              Buy
+            </Link>
           </div>
         </div>
+      </div>
 
-        <div className="ticket_svg">
-          <div className="ticket_content">
-            <p>
-              This is a {props.ticketType} ticket to FooFest 2022 - Jazz festival. The ticket cost
-              {props.ticketPrice} DKK, and is a big nice festival with big artist.
-            </p>
-            <div className="ticket_btn">
-              <button type="primary" onClick={handleClick}>
-                Read less
-              </button>
-              OR
-              <Link onClick={addTicketTypeToBasket} to="/basket">
-                Buy
-              </Link>
-            </div>
+      {/* <div className="ticket_svg">
+        <div className="ticket_content">
+          <p>
+            This is a {props.ticketType} ticket to FooFest 2022 - Jazz festival. The ticket cost
+            {props.ticketPrice} DKK, and is a big nice festival with big artist.
+          </p>
+          <div className="ticket_btn">
+            <button type="primary" onClick={handleClick}>
+              Read less
+            </button>
+            OR
+            <Link
+              onClick={(e) => {
+                party.confetti(e.target, { count: party.variation.range(100, 200) });
+                addTicketTypeToBasket();
+              }}
+              to="/basket"
+            >
+              Buy
+            </Link>
           </div>
         </div>
-      </ReactCardFlip>
+      </div> */}
+      {/* </ReactCardFlip> */}
     </article>
   );
 }
--- src/components/booking/TicketInBasket.jsx
+++ src/components/booking/TicketInBasket.jsx
@@ -1,46 +1,46 @@
-import { useState, useContext } from "react";
-import ReactCardFlip from "react-card-flip";
+import { useContext } from "react";
+// import ReactCardFlip from "react-card-flip";
 
 import { TicketBasketContext } from "../../contexts/TicketBasketContext";
 
 export default function TicketInBasket(props) {
-  const [isFlipped, setIsFlipped] = useState(false);
+  // const [isFlipped, setIsFlipped] = useState(false);
   const { ticketBasket } = useContext(TicketBasketContext);
 
-  function handleClick(e) {
-    setIsFlipped((prevState) => !prevState);
-  }
+  // function handleClick(e) {
+  //   setIsFlipped((prevState) => !prevState);
+  // }
 
   return (
     <article className="ticket">
-      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
-        <div className="ticket_svg">
-          <div className="ticket_content">
-            <h2>{ticketBasket.ticketType}</h2>
-            <h3>FooFest 2022</h3>
-            <h4>{ticketBasket.ticketPrice} DKK</h4>
-            <div className="ticket_btn">
-              <button type="primary" onClick={handleClick}>
-                Read more
-              </button>
-            </div>
+      {/* <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical"> */}
+      <div className="ticket_svg">
+        <div className="ticket_content">
+          <h2>{ticketBasket.ticketType}</h2>
+          <h3>FooFest 2022</h3>
+          <h4>{ticketBasket.ticketPrice} DKK</h4>
+          <div className="ticket_btn">
+            {/* <button type="primary" onClick={handleClick}>
+              Read more
+            </button> */}
           </div>
         </div>
+      </div>
 
-        <div className="ticket_svg">
-          <div className="ticket_content">
-            <p>
-              This is a {props.ticketType} ticket to FooFest 2022 - Jazz festival. The ticket cost{" "}
-              {props.ticketPrice} DKK, and is a big nice festival with big artist.
-            </p>
-            <div className="ticket_btn">
-              <button type="primary" onClick={handleClick}>
-                Read less
-              </button>
-            </div>
+      {/* <div className="ticket_svg">
+        <div className="ticket_content">
+          <p>
+            This is a {props.ticketType} ticket to FooFest 2022 - Jazz festival. The ticket cost{" "}
+            {props.ticketPrice} DKK, and is a big nice festival with big artist.
+          </p>
+          <div className="ticket_btn">
+            <button type="primary" onClick={handleClick}>
+              Read less
+            </button>
           </div>
         </div>
-      </ReactCardFlip>
+      </div> */}
+      {/* </ReactCardFlip> */}
     </article>
   );
 }
