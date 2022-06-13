4770fa0a955ba539acdb0c430d45a174bf5d3cf9
Magnus <magn902m@stud.kea.dk>
2022-06-13 18:32:43 +0200

    styling and layout

--- src/components/schedule/SingleBand.jsx
+++ src/components/schedule/SingleBand.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 import { BandDataContext } from "../../contexts/BandDataContext";
 
@@ -7,6 +7,7 @@ export default function SingleBand(props) {
   const { bandData } = useContext(BandDataContext);
   const [singleBandS, setSingleBandS] = useState([]);
   const params = useParams();
+  const navigate = useNavigate();
   // console.log(params);
 
   // useEffect(() => {
@@ -29,53 +30,53 @@ export default function SingleBand(props) {
   // console.log(singleBand);
 
   return (
-    <main>
-      <section>
-        <h2 className="popup_heading">{singleBand.name}</h2>
+    <main id="single_band">
+      <section className="btn_name_time">
+        <button className="back_btn" onClick={() => navigate(-1)}>
+          Back
+        </button>
+        <h2>{singleBand.name}</h2>
+        <p className="import_info">
+          {singleBand.stage} / {singleBand.act.start}-{singleBand.act.end} / {singleBand.day}
+        </p>
+      </section>
 
-        <article className="grid_desktop">
-          <figure>
-            <img
-              src={
-                singleBand.logo.indexOf("http://")
-                  ? `https://prototype-masters-foofest.herokuapp.com/logos/${singleBand.logo}`
-                  : singleBand.logo
-              }
-              alt={singleBand.name}
-            />
-            {singleBand.logoCredits ? <figcaption>{singleBand.logoCredits}</figcaption> : null}
+      <section className="single_band_img">
+        <figure>
+          <img
+            src={
+              singleBand.logo.indexOf("http://")
+                ? `https://prototype-masters-foofest.herokuapp.com/logos/${singleBand.logo}`
+                : singleBand.logo
+            }
+            alt={singleBand.name}
+          />
+          {singleBand.logoCredits ? <figcaption>{singleBand.logoCredits}</figcaption> : null}
+        </figure>
+      </section>
 
-            <div className="ribbon">
-              <span>{singleBand.genre}</span>
-            </div>
-          </figure>
-          <div className="top_info">
+      <section className="more_info">
+        <article className="genre_members">
+          <div className="members">
             <p>
-              <span className="bold">Stage:</span> {singleBand.stage} - {singleBand.day}
+              <strong>Band members:</strong>
             </p>
-            <p>
-              <span className="bold">Time-slot:</span> {singleBand.act.start}-{singleBand.act.end}
-            </p>
+            <ul>
+              {singleBand.members.map((member) => (
+                <li key={Math.random()} className="member">
+                  <p>{member}</p>
+                </li>
+              ))}
+            </ul>
           </div>
-        </article>
 
-        <article className="band_members">
-          {singleBand.members.map((member) => (
-            <div key={Math.random()} className="member">
-              <p>{member}</p>
-            </div>
-          ))}
-        </article>
-
-        <div className="bio_container">
-          <p>
-            <span className="bold">BIO: </span>
-            {singleBand.bio}
+          <p className="genre">
+            <strong>Genre: </strong>
+            <span>{singleBand.genre}</span>
           </p>
-          <i>
-            <figcaption>Image credits: {singleBand.logoCredits}</figcaption>
-          </i>
-        </div>
+        </article>
+
+        <p className="desciption">{singleBand.bio}</p>
       </section>
     </main>
   );
