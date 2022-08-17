7329e7f727ebc75e108b7324f0d414e1205f0917
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-08-17 14:43:34 +0200

    card updates

--- src/components/booking/Ticket.jsx
+++ src/components/booking/Ticket.jsx
@@ -1,13 +1,13 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 // import ReactCardFlip from "react-card-flip";
 import { Link } from "react-router-dom";
 import party from "party-js";
-
 import { TicketBasketContext } from "../../contexts/TicketBasketContext";
 
 export default function Ticket(props) {
   // const [isFlipped, setIsFlipped] = useState(false);
   const { setTicketBasket } = useContext(TicketBasketContext);
+  const [flip, setFlip] = useState(false);
 
   // function handleClick(e) {
   //   setIsFlipped((prevState) => !prevState);
@@ -31,63 +31,46 @@ export default function Ticket(props) {
 
   return (
     <article className="ticket">
-      {/* We are using React-card-flip and it only works if there is two boxing inside it. */}
-      {/* <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical"> */}
-      <div className="ticket_svg">
-        <div className="ticket_content">
-          <h2>{props.ticketType}</h2>
-          <h3>FooFest 2022</h3>
-          <h4>{props.ticketPrice} DKK</h4>
-          <div className="ticket_btn">
-            {/* If the btn is clicked, the card will turn to the other side */}
-            {/* <button type="primary" onClick={handleClick}>
-              Read more
-            </button>
-            OR */}
-            {/* Here are we adding the selected ticket to the ticketBasket, and going the the basket page. */}
-            <Link
-              onClick={(e) => {
-                party.confetti(e.target, {
-                  shapes: ["star"],
-                  count: party.variation.range(50, 200),
-                  size: party.variation.range(1, 2),
-                  speed: party.variation.range(100, 1000),
-                });
-                addTicketTypeToBasket();
-              }}
-              className="btn"
-              to="/basket"
-            >
-              Buy
-            </Link>
+      <div
+        className={`card ${flip ? `flip` : ""}`}
+        style={{
+          backgroundColor: props.ticketInfo.color,
+        }}
+        onClick={() => setFlip(!flip)}
+      >
+        <div className="front">
+          <img src={props.ticketInfo.img} alt={props.ticketInfo.img} />
+          <h4>FOO FESTIVAL</h4>⁄ <h2>{props.ticketInfo.ticketType}</h2>
+          {/* trying to map through li items, not working, coming out in the console */}
+          <ul>
+            {console.log("all info", props.ticketInfo.info)}
+            {props.ticketInfo.info.map((old) => {
+              return <li>{old}</li>;
+            })}
+          </ul>
+          <div className="flex_card">
+            <h3>PRICE: {props.ticketInfo.ticketPrice}</h3>
+            <div className="ticket_btn">
+              <Link
+                onClick={(e) => {
+                  party.confetti(e.target, {
+                    shapes: ["star"],
+                    count: party.variation.range(50, 200),
+                    size: party.variation.range(1, 2),
+                    speed: party.variation.range(100, 1000),
+                  });
+                  addTicketTypeToBasket();
+                }}
+                className="btn"
+                to="/basket"
+              >
+                BUY TICKET
+              </Link>
+            </div>
           </div>
         </div>
+        <div className="back"></div>
       </div>
-
-      {/* <div className="ticket_svg">
-        <div className="ticket_content">
-          <p>
-            This is a {props.ticketType} ticket to FooFest 2022 - Jazz festival. The ticket cost
-            {props.ticketPrice} DKK, and is a big nice festival with big artist.
-          </p>
-          <div className="ticket_btn">
-            <button type="primary" onClick={handleClick}>
-              Read less
-            </button>
-            OR
-            <Link
-              onClick={(e) => {
-                party.confetti(e.target, { count: party.variation.range(100, 200) });
-                addTicketTypeToBasket();
-              }}
-              to="/basket"
-            >
-              Buy
-            </Link>
-          </div>
-        </div>
-      </div> */}
-      {/* </ReactCardFlip> */}
     </article>
   );
 }
